69835f6464551de0bb432846a4cc27c2a1e92b3c
hngda01 <hngda01@gmail.com>
2017-11-07 15:13:56 +0700

    basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "hello, world!"
-  end
-  def hi
-  	render html: "hi, my name is hai"
-  end
+  include SessionsHelper
 end
